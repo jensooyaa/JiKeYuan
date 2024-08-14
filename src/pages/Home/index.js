d7ff853582016271ec3f8be33db8b63b6d30f984
2749605017@qq.com <2749605017@qq.com>
2024-08-14 16:13:50 +0800

    拓展-API模块封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,25 @@
-import { useEffect, useRef } from 'react'
-import * as echarts from 'echarts'
-
+import { BarChart } from "./conponents/BarChart"
 const Home = () => {
-    const chartRef = useRef(null)
-    useEffect(() => {
-        // 1. 生成实例
-        const myChart = echarts.init(chartRef.current)
-        // 2. 准备图表参数
-        const option = {
-            xAxis: {
-                type: 'category',
-                data: ['Vue', 'angular', 'React'],
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    data: [30, 30, 40],
-                    type: 'bar'
-                }
-            ]
-        }
-        // 3. 渲染参数
-        myChart.setOption(option)
-    }, [])
-
     return (
+
         <div>
-            <div ref={chartRef} style={{ width: '400px', height: '300px' }} />
+            <div>
+                <div style={{ fontWeight: 'bold', fontSize: '20px' }}>三大框架使用度</div>
+                <BarChart
+                    xData={['Vue', 'React', 'Angular']}
+                    sData={[2000, 5000, 1000]} >
+
+                </BarChart>
+            </div>
+            <div>
+                <div style={{ fontWeight: 'bold', fontSize: '20px' }}>三大框架满意度</div>
+                <BarChart
+                    xData={['Vue', 'React', 'Angular']}
+                    sData={[200, 500, 100]}
+                    style={{ width: '500px', height: '400px' }}>
+                </BarChart>
+            </div>
+
         </div >
     )
 }
--- src/store/mudules/user.js
+++ src/store/mudules/user.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { request, getToken, setToken as setTokens } from '../../utils'
 import { clearToken } from '../../utils'
+import { loginAPI, getProfileAPI } from '../../apis/user'
 const userStore = createSlice({
     name: 'user',
     // 数据状态
@@ -34,13 +35,13 @@ const userReducer = userStore.reducer
 // 异步方法封装
 const fetchLogin = (loginForm) => {
     return async (dispatch) => {
-        const res = await request.post('/authorizations', loginForm)
+        const res = await loginAPI(loginForm)
         dispatch(setToken(res.data.token))
     }
 }
 const fetchUserInfo = () => {
     return async (dispatch) => {
-        const res = await request.get('/user/profile')
+        const res = await getProfileAPI()
         dispatch(setUserInfo(res.data))
     }
 }
