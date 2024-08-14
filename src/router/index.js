48ba88973a609837083d4402aeadbbe3d4fd4927
2749605017@qq.com <2749605017@qq.com>
2024-08-14 09:28:28 +0800

    Layout-二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,27 @@ import { createBrowserRouter } from 'react-router-dom'
 import Login from '../pages/Login'
 import Layout from '../pages/Layout'
 import AuthRoute from '../components/AuthRoute'
+import Home from '../pages/Home'
+import Article from '../pages/Article'
+import Publish from '../pages/Publish'
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <AuthRoute><Layout /></AuthRoute>
+        element: <AuthRoute><Layout /></AuthRoute>,
+        children: [
+            {
+                path: "Home",
+                element: <Home />
+            },
+            {
+                path: "Article",
+                element: <Article />
+            },
+            {
+                path: "Publish",
+                element: <Publish />
+            }
+        ]
     },
     {
         path: "/Login",
