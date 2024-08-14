d12eba31a0af0d4454f3adda652a9132b0dea7ce
2749605017@qq.com <2749605017@qq.com>
2024-08-14 15:36:07 +0800

    Home-Echarts组件封装实现

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,36 @@
+import { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
 const Home = () => {
-    return <div>this is home</div>
+    const chartRef = useRef(null)
+    useEffect(() => {
+        // 1. 生成实例
+        const myChart = echarts.init(chartRef.current)
+        // 2. 准备图表参数
+        const option = {
+            xAxis: {
+                type: 'category',
+                data: ['Vue', 'angular', 'React'],
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [30, 30, 40],
+                    type: 'bar'
+                }
+            ]
+        }
+        // 3. 渲染参数
+        myChart.setOption(option)
+    }, [])
+
+    return (
+        <div>
+            <div ref={chartRef} style={{ width: '400px', height: '300px' }} />
+        </div >
+    )
 }
+
 export default Home
